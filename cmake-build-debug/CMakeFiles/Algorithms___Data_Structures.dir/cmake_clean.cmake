47fc3b69c3a5ef8202862f99651b9a012e468674
aldo-manco-jr <aldo.manco@protonmail.ch>
2021-01-06 20:41:05 +0100

    math_algorithms_added_cmake_added_java_code_added

---
+++ cmake-build-debug/CMakeFiles/Algorithms___Data_Structures.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Algorithms___Data_Structures.exe"
+  "Algorithms___Data_Structures.pdb"
+  "CMakeFiles/Algorithms___Data_Structures.dir/main.c.o"
+  "libAlgorithms___Data_Structures.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Algorithms___Data_Structures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
